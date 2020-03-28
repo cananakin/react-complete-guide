9e430339c8e54129a39ddb7723147de717640324
canan <e.cananakin@gmail.com>
2020-03-28 12:58:14 +0000

    Burger Builder

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,23 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 import classes from './Burger.css'
 
 const Burger = (props) => {
+    
+    let transformedIngredient = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            })
+        }).reduce((arr, el) => {
+            return arr.concat(el)
+        }, [])
+    
+    if(transformedIngredient.length === 0 ){
+        transformedIngredient = <p>Please Start Adding ingredients</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top' />
-            <BurgerIngredient type='cheese' />
-            <BurgerIngredient type='meat' />
+            {transformedIngredient}
             <BurgerIngredient type='bread-bottom' />
         </div>
     )
